76faa7f9afbe527f629f7f40c191025c6dff49e9
Alex Keh <alexkeh@users.noreply.github.com>
2018-03-07 17:06:48 -0800

    Added error handling and object disposal
    Added copyright, error handling, and object disposal

--- samples/dotnet-core/DataReader.cs
+++ samples/dotnet-core/DataReader.cs
@@ -1,3 +1,23 @@
+
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
 using System;
 using Oracle.ManagedDataAccess.Client;
 
@@ -6,39 +26,57 @@ namespace ODPCoreDataReader
     class Program
     {
         static void Main(string[] args)
-        {
-            //Demo: Basic ODP.NET Core application
-            //Create a connection to Oracle			
-            string conString = "User Id=hr;Password=<password>;" +
+        {          
+            //Demo: Basic ODP.NET Core application to connect, query, and return
+            // results from an OracleDataReader to a console
 
-            //How to connect to an Oracle DB without SQL*Net configuration 
-            //  file also known as tnsnames.ora.
+            //Create a connection to Oracle			
+            string conString = "Pooling=false;User Id=hr;Password=<password>;" +
+
+            //How to connect to an Oracle DB without SQL*Net configuration file
+            //  also known as tnsnames.ora.
             "Data Source=<ip or hostname>:1521/<service name>;";
 
             //How to connect to an Oracle DB with a DB alias.
             //Uncomment below and comment above.
             //"Data Source=<service name alias>;";
-
-            OracleConnection con = new OracleConnection();
-            con.ConnectionString = conString;
-            con.Open();
-
-            //Create a command within the context of the connection
-            //Use the command to display employee names from 
-            // the EMPLOYEES table
-            OracleCommand cmd = con.CreateCommand();
-            cmd.CommandText = "select first_name from employees where department_id = 50";
-
-            //Execute the command and use DataReader to display the data
-            OracleDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+     
+            using (OracleConnection con = new OracleConnection(conString))
             {
-                Console.WriteLine("Employee Name: " + reader.GetString(0));
-            }
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
 
-            Console.WriteLine();
-            Console.WriteLine("Press 'Enter' to continue");
-            Console.ReadLine();
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select first_name from employees where department_id = :id";
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", 50);
+                        cmd.Parameters.Add(id);
+                        cmd.BindByName = true;
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("Employee First Name: " + reader.GetString(0));
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Enter' to continue");
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
