ef37a979d0909c1a82b263849f5d9217efa57ad4
Alex Keh <alexkeh@users.noreply.github.com>
2018-03-07 17:28:31 -0800

    Create Startup.cs

---
+++ samples/dotnet-core/asp.net-core/Startup.cs
@@ -0,0 +1,102 @@
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ODPCoreASPCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                //Demo: Basic ODP.NET Core application for ASP.NET Core
+                // to connect, query, and return results to a web page
+
+                //Create a connection to Oracle			
+                //string conString = "Pooling=false;User Id=hr;Password=<password>;" +
+                string conString = "Pooling=false;User Id=hr;Password=hr;" +
+
+                //How to connect to an Oracle DB without SQL*Net configuration file
+                //  also known as tnsnames.ora.
+                //"Data Source=<ip or hostname>:1521/<service name>;";
+                "Data Source=localhost:1521/orclpdb;";
+
+                //How to connect to an Oracle DB with a DB alias.
+                //Uncomment below and comment above.
+                //"Data Source=<service name alias>;";
+
+                using (OracleConnection con = new OracleConnection(conString))
+                {
+                    using (OracleCommand cmd = con.CreateCommand())
+                    {
+                        try
+                        {
+                            con.Open();
+
+                            //Use the command to display employee names from 
+                            // the EMPLOYEES table
+                            cmd.CommandText = "select first_name from employees where department_id = :id";
+
+                            // Assign id to the department number 50 
+                            OracleParameter id = new OracleParameter("id", 50);
+                            cmd.Parameters.Add(id);
+                            cmd.BindByName = true;
+
+                            //Execute the command and use DataReader to display the data
+                            OracleDataReader reader = cmd.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                await context.Response.WriteAsync("Employee First Name: " + reader.GetString(0) + "\n");
+                            }
+
+                            reader.Dispose();
+                        }
+                        catch (Exception ex)
+                        {
+                            await context.Response.WriteAsync(ex.Message);
+                        }
+                    }
+                }
+
+            });
+        }
+    }
+}
