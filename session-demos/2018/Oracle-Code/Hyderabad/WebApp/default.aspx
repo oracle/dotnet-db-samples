d7c005c84fcb5518c3137e84b82a2b7749ab158a
Alex Keh <alexkeh@users.noreply.github.com>
2018-04-06 16:47:54 +0530

    Add files via upload

---
+++ session-demos/2018/Oracle-Code/Hyderabad/WebApp/default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication16._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Welcome to my HR app!<br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ session-demos/2018/Oracle-Code/Hyderabad/WebApp/default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace WebApplication
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string constr = "user id=hr;password=[password];Data Source=pdb";
+
+            try
+            {
+                OracleConnection con = new OracleConnection(constr);
+                con.Open();
+
+                string sql = "select * from employees";
+                OracleCommand cmd = new OracleCommand(sql, con);
+                cmd.CommandType = CommandType.Text;
+                OracleDataAdapter da = new OracleDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                TextBox1.Text = ex.Message;
+            }
+
+        }
+    }
+}
