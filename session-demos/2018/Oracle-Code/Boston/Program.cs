bd182893ddd4148b95334c8c3b25b2193de66079
Christian Shay <christian.shay@gmail.com>
2018-04-17 01:08:20 -0400

    Create Program.cs

---
+++ session-demos/2018/Oracle-Code/Boston/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace testcommandlineapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                //Needed only for Oracle Application Container Cloud Service
+                //.UseUrls("http://0.0.0.0:"+ Environment.GetEnvironmentVariable("PORT"))
+                .Build();
+    }
+}
