cdaf8eb882e8436fd268c90516b72e889d003355
Alex Keh <alexkeh@users.noreply.github.com>
2018-07-31 13:50:18 -0700

    Create config-api.cs
    Sample ODP.NET Core Configuration API usage

---
+++ samples/dotnet-core/configuration-api/config-api.cs
@@ -0,0 +1,90 @@
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ODP_Core_Config_API
+{
+    class odp_core_config
+    {
+        static void Main(string[] args)
+        {
+            // This sample demonstrates how to use ODP.NET Core Configuration API
+
+            // Add connect descriptors and net service names entires.
+            OracleConfiguration.OracleDataSources.Add("orclpdb", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=<hostname or IP>)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=<service name>)(SERVER=dedicated)))");
+            OracleConfiguration.OracleDataSources.Add("orcl", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=<hostname or IP>)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=<service name>)(SERVER=dedicated)))");
+
+            // Set default statement cache size to be used by all connections.
+            OracleConfiguration.StatementCacheSize = 25;
+
+            // Disable self tuning by default.
+            OracleConfiguration.SelfTuning = false;
+
+            // Bind all parameters by name.
+            OracleConfiguration.BindByName = true;
+
+            // Set default timeout to 60 seconds.
+            OracleConfiguration.CommandTimeout = 60;
+
+            // Set default fetch size as 1 MB.
+            OracleConfiguration.FetchSize = 1024 * 1024;
+
+            // Set tracing options
+            OracleConfiguration.TraceLevel = 7;
+            OracleConfiguration.TraceOption = 1;
+            // Uncomment below to write traces to specified directory
+            //OracleConfiguration.TraceFileLocation = @"D:\traces";
+
+            // Set network properties
+            OracleConfiguration.SendBufferSize = 8192;
+            OracleConfiguration.ReceiveBuffereSize = 8192;
+            OracleConfiguration.DisableOOB = true;
+
+            OracleConnection orclCon = null;
+
+            try
+            {
+                // Open a connection
+                orclCon = new OracleConnection("user id=hr; password=<password>; data source=orclpdb");
+                orclCon.Open();
+
+                // Execute simple select statement that returns first 10 names from EMPLOYEES table
+                OracleCommand orclCmd = orclCon.CreateCommand();
+                orclCmd.CommandText = "select first_name from employees where rownum <= 10 ";
+                OracleDataReader rdr = orclCmd.ExecuteReader();
+
+                while (rdr.Read())
+                    Console.WriteLine("Employee Name: " + rdr.GetString(0));
+
+                Console.ReadLine();
+
+                rdr.Dispose();
+                orclCmd.Dispose();
+            }
+            finally
+            {
+                // Close the connection
+                if (null != orclCon)
+                    orclCon.Close();
+            }
+        }
+    }
+}
