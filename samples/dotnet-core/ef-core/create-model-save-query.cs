0174cfbc8b68af71a5292c3e19cb84dbf41df977
Alex Keh <alexkeh@users.noreply.github.com>
2018-10-03 13:23:36 -0700

    Create create-model-save-query.cs

---
+++ samples/dotnet-core/ef-core/create-model-save-query.cs
@@ -0,0 +1,74 @@
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
+/* Copyright (c) .NET Foundation and Contributors                         */
+
+/******************************************************************************
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ *****************************************************************************/
+ 
+using System.Collections.Generic;
+using Oracle.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OracleEFCore
+{
+    class Program
+    {
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseOracle(@"User Id=blog;Password=blog;Data Source=<ip or hostname>:1521/<service name>");
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Url { get; set; }
+            //public int Rating { get; set; }
+            public List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+            using (var db = new BloggingContext())
+            {
+                var blog = new Blog { Url = "https://blogs.oracle.com" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+            using (var db = new BloggingContext())
+            {
+                var blogs = db.Blogs;
+            }
+
+        }
+
+    }
+}
