97c0720ce65ddebe171bf9296510d24f9ab64de1
Christian Shay <christian.shay@gmail.com>
2020-02-23 17:05:20 -0800

    Create DataReader.cs

---
+++ video-demos/IntroVisualStudioAlwaysFreeADB/DataReader.cs
@@ -0,0 +1,75 @@
+/* Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace VSADBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demo: Basic ODP.NET Managed application to connect, query, and return
+            // results from an OracleDataReader to a console
+
+            //Create a connection to Oracle			
+            string conString = "User Id=hr;Password=mypassword;" +
+
+            "Data Source=myalias>;";
+
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        //Use the command to display employee names from 
+                        // the EMPLOYEES table
+                        cmd.CommandText = "select first_name from employees where department_id = :id";
+
+                        // Assign id to the department number 50 
+                        OracleParameter id = new OracleParameter("id", 50);
+                        cmd.Parameters.Add(id);
+
+                        //Execute the command and use DataReader to display the data
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("Employee First Name: " + reader.GetString(0));
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Enter' to continue");
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
