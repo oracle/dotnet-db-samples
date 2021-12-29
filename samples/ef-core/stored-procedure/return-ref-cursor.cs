b3f29d84c8700475ae4420ce09b9a4aa71b84f63
Alex Keh <alex.keh@oracle.com>
2021-12-29 15:44:20 -0800

    Update and rename samples/dotnet-core/ef-core/stored-procedure/return-ref-cursor.cs to samples/ef-core/stored-procedure/return-ref-cursor.cs

--- samples/ef-core/stored-procedure/return-ref-cursor.cs
+++ samples/ef-core/stored-procedure/return-ref-cursor.cs
@@ -1,22 +1,3 @@
-/* Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved. */
-
-/******************************************************************************
- *
- * You may not use the identified files except in compliance with The MIT
- * License (the "License.")
- *
- * You may obtain a copy of the License at
- * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *****************************************************************************/
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
@@ -102,3 +83,22 @@ namespace OracleEFCore
         }
     }
 }
+
+/* Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
