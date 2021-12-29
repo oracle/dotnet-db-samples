08d25929a45143b4b76560c1cffc5c2a6cca9aa6
Alex Keh <alex.keh@oracle.com>
2021-12-29 11:18:00 -0800

    Update and rename samples/dotnet-core/configuration-api/configuration-class.cs to samples/configuration-api/configuration-class.cs

--- samples/configuration-api/configuration-class.cs
+++ samples/configuration-api/configuration-class.cs
@@ -1,22 +1,3 @@
-/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
-
-/******************************************************************************
- *
- * You may not use the identified files except in compliance with The MIT
- * License (the "License.")
- *
- * You may obtain a copy of the License at
- * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *****************************************************************************/
-
 using System;
 using Oracle.ManagedDataAccess.Client;
 
@@ -88,3 +69,22 @@ namespace ODP_Core_Config_API
         }
     }
 }
+
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with The MIT
+ * License (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
