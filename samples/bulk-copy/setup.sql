21912bcd13577358f7f636ee10d0797a7f88b16d
Alex Keh <alex.keh@oracle.com>
2022-02-26 19:56:58 -0800

    Create setup.sql

---
+++ samples/bulk-copy/setup.sql
@@ -0,0 +1,3 @@
+  /* This table is used by the ODP.NET Buulk Copy sample code. */
+  /* Run this script to create the table. Populate it with a few rows. Then, run the ODP.NET sample. */
+  CREATE TABLE "BLOGS" ("ID" NUMBER, "URL" VARCHAR2(50) NOT NULL ENABLE);
