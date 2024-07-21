46aff191ac2c371c1d847b8b34f1251c39871716
Alex Keh <alex.keh@oracle.com>
2024-07-20 18:59:25 -0700

    Update ArrayBind.cs
    Updated sample to array bind 4 rows instead of 3 to avoid a square example as per customer suggestion.
    https://github.com/oracle/dotnet-db-samples/issues/368
    
    Updated license link.
    
    Cleaned up comments and code artifacts for clarity.

--- samples/array-bind/ArrayBind.cs
+++ samples/array-bind/ArrayBind.cs
@@ -2,116 +2,114 @@ using System;
 using System.Data;
 using Oracle.ManagedDataAccess.Client;
 
-namespace ODPSample
+namespace ODPArrayBind
 {
-  /// <summary>
-  /// Sample: Demonstrates array binding
-  /// </summary>
-  class ArrayBind
-  {
-    static void Main(string[] args)
+    /// <summary>
+    /// Sample: Demonstrates ODP.NET array binding
+    /// </summary>
+    class ArrayBind
     {
-      // Connect
-      // This sample code's DEPT table shares the same characteristics as SCOTT schema's DEPT table
-      string connectStr = "User Id=<USER ID>;Password=<PASSWORD>;Data Source=<TNS ALIAS>";
+        static void Main(string[] args)
+        {
+            // Connect
+            // This sample code's DEPT table shares the same characteristics as the SCOTT schema's DEPT table.
+            string connectStr = "User Id=<USER ID>;Password=<PASSWORD>;Data Source=<TNS ALIAS>";
 
-      // Setup the Tables for sample
-      Setup(connectStr);
+            // Clear rows from past sample code executions
+            Setup(connectStr);
 
-      // Initialize array of data
-      int[]    myArrayDeptNo   = new int[3]{1, 2, 3};
-      String[] myArrayDeptName = {"Dev", "QA", "Facility"};
-      String[] myArrayDeptLoc  = {"New York", "Maryland", "Texas"};
-      
-      OracleConnection connection = new OracleConnection(connectStr);
-      OracleCommand    command    = new OracleCommand (
-        "insert into dept values (:deptno, :deptname, :loc)", connection);
-          
-      // Set the Array Size to 3. This applied to all the parameter in 
-      // associated with this command
-      command.ArrayBindCount = 3;
+            // Initialize array of data
+            int[] myArrayDeptNo = new int[4] { 1, 2, 3, 4 };
+            String[] myArrayDeptName = { "Dev", "QA", "PM", "Integration" };
+            String[] myArrayDeptLoc = { "California", "Arizona", "Texas", "Oregon" };
 
-      // deptno parameter
-      OracleParameter deptNoParam = new OracleParameter("deptno",OracleDbType.Int32);
-      deptNoParam.Direction       = ParameterDirection.Input;
-      deptNoParam.Value           = myArrayDeptNo;
-      command.Parameters.Add(deptNoParam);
+            OracleConnection connection = new OracleConnection(connectStr);
+            OracleCommand command = new OracleCommand(
+              "insert into dept values (:deptno, :deptname, :loc)", connection);
 
-      // deptname parameter
-      OracleParameter deptNameParam = new OracleParameter("deptname", OracleDbType.Varchar2);
-      deptNameParam.Direction       = ParameterDirection.Input;
-      deptNameParam.Value           = myArrayDeptName;
-      command.Parameters.Add(deptNameParam);
+            // Set the array size to 4. This applies to all the command's associated parameters.
+            command.ArrayBindCount = 4;
 
-      // loc parameter
-      OracleParameter deptLocParam = new OracleParameter("loc", OracleDbType.Varchar2);
-      deptLocParam.Direction       = ParameterDirection.Input;
-      deptLocParam.Value           = myArrayDeptLoc;
-      command.Parameters.Add(deptLocParam);
+            // Deptno parameter
+            OracleParameter deptNoParam = new OracleParameter("deptno", OracleDbType.Int32);
+            deptNoParam.Direction = ParameterDirection.Input;
+            deptNoParam.Value = myArrayDeptNo;
+            command.Parameters.Add(deptNoParam);
 
-      try 
-      {
-        connection.Open();
-        command.ExecuteNonQuery();
-        Console.WriteLine("{0} Rows Inserted", command.ArrayBindCount);
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine("Execution Failed:" + e.Message);
-      }
-      finally
-      {
-        // connection, command used server side resource, dispose them
-        // asap to conserve resource
-        connection.Close();
-        command.Dispose();
-        connection.Dispose();
-      }
+            // Deptname parameter
+            OracleParameter deptNameParam = new OracleParameter("deptname", OracleDbType.Varchar2);
+            deptNameParam.Direction = ParameterDirection.Input;
+            deptNameParam.Value = myArrayDeptName;
+            command.Parameters.Add(deptNameParam);
+
+            // Loc parameter
+            OracleParameter deptLocParam = new OracleParameter("loc", OracleDbType.Varchar2);
+            deptLocParam.Direction = ParameterDirection.Input;
+            deptLocParam.Value = myArrayDeptLoc;
+            command.Parameters.Add(deptLocParam);
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("{0} rows inserted", command.ArrayBindCount);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Execution failed:" + e.Message);
+            }
+            finally
+            {
+                // Dispose connection and command used server side resource
+                connection.Close();
+                command.Dispose();
+                connection.Dispose();
+            }
+        }
+
+        public static void Setup(string connectStr)
+        {
+            int[] myArrayDeptNo = new int[4] { 1, 2, 3, 4 };
+
+            OracleConnection conn = new OracleConnection(connectStr);
+            OracleCommand cmd = new OracleCommand("delete dept where deptno = :1", conn);
+
+            // Bind with a 4 item array
+            cmd.ArrayBindCount = 4;
+
+            OracleParameter param1 = new OracleParameter();
+            param1.OracleDbType = OracleDbType.Int32;
+            param1.Value = myArrayDeptNo;
+
+            cmd.Parameters.Add(param1);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Setup Failed:{0}", e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                cmd.Dispose();
+            }
+        }
     }
-
-    public static void Setup(string connectStr)
-    {
-      int[]  myArrayDeptNo   = new int[3]{1, 2, 3};
-
-      OracleConnection conn = new OracleConnection(connectStr);
-      OracleCommand cmd     = new OracleCommand("delete dept where deptno = :1", conn);
-
-      // Bind with an array of 3 items
-      cmd.ArrayBindCount = 3;
-
-      OracleParameter param1 = new OracleParameter();
-      param1.OracleDbType  = OracleDbType.Int32;
-      param1.Value = myArrayDeptNo;
-
-      cmd.Parameters.Add(param1);
-
-      try
-      {
-        conn.Open();
-        cmd.ExecuteNonQuery(); 
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine("Setup Failed:{0}" ,e.Message);
-      }
-      finally
-      {
-        conn.Close();
-        cmd.Dispose();
-      }
-    }
-  }
 }
 
-/* Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved. */
- 
+/* Copyright (c) 2017, 2024 Oracle and/or its affiliates. All rights reserved. */
+
 /******************************************************************************
  *
  * You may not use the identified files except in compliance with The MIT
  * License (the "License.")
  *
  * You may obtain a copy of the License at
- * https://github.com/oracle/Oracle.NET/blob/master/LICENSE
+ * https://github.com/oracle/Oracle.NET/blob/master/LICENSE.txt
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
